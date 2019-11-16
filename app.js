5d1c015d19b4ef660984ae9196031cdefada7de4
L-Garay <garay.logan@gmail.com>
2019-11-15 21:32:49 -0700

    counting right

--- app.js
+++ app.js
@@ -1,40 +1,91 @@
-let orcs = {
-  type: "orc",
-  orcsSlain: 0
-};
+let orcsSlain = 0;
 let killElem = document.querySelector("#kill-count");
+let swordPrice = document.querySelector("#sword-price");
+let bowPrice = document.querySelector("#bow-price");
+let spikesPrice = document.querySelector("#spikes-price");
 
 function kill() {
-  orcs.orcsSlain++;
+  orcsSlain++;
+  orcsSlain += clickUpgrades.sword.quantity * clickUpgrades.sword.damage;
+  orcsSlain += clickUpgrades.bow.quantity * clickUpgrades.bow.damage;
+  orcsSlain += clickUpgrades.spikes.quantity * clickUpgrades.spikes.damage;
+
   countKills();
+  updateSwords();
 }
 function countKills() {
-  killElem.innerText = orcs.orcsSlain;
+  killElem.innerText = orcsSlain;
+}
+function updateSwords() {
+  if (clickUpgrades.sword.price[0]) {
+    swordPrice.innerText = clickUpgrades.sword.price[0];
+  } else {
+    swordPrice.innerText = clickUpgrades.sword.price[1];
+  }
 }
 
-let upgrades = {
+let clickUpgrades = {
   sword: {
-    price: [10, 50, 100, 200],
-    mod: [2, 5, 20, 50],
-    modIndex: 0,
-    priceIndex: 0
+    price: [10, 50],
+    damage: 2,
+    quantity: 0
   },
   bow: {
-    price: [20, 75, 150, 350],
-    mod: [5, 20, 50, 100],
-    modIndex: 0,
-    priceIndex: 0
+    price: [20, 75],
+    damage: 5,
+    quantity: 0
   },
   spikes: {
-    price: [50, 150, 400, 650],
-    mod: [10, 40, 100, 150],
-    modIndex: 0,
-    priceIndex: 0
-  },
-  companions: {
-    price: [100],
-    mod: [20],
-    modIndex: 0,
-    priceIndex: 0
+    price: [50, 150],
+    damage: 40,
+    quantity: 0
   }
 };
+let autoUpgrades = {
+  companions: {
+    price: 150,
+    damage: 30,
+    quantity: 0
+  }
+};
+
+function buysword() {
+  clickUpgrades.sword.quantity++;
+  if (clickUpgrades.sword.quantity <= 10) {
+    orcsSlain -= clickUpgrades.sword.price[0];
+  } else {
+    orcsSlain -= clickUpgrades.sword.price[1];
+  }
+  countKills();
+  updateSwords();
+}
+function buybow() {
+  clickUpgrades.bow.quantity++;
+  if (clickUpgrades.bow.quantity <= 10) {
+    orcsSlain -= clickUpgrades.bow.price[0];
+  } else {
+    orcsSlain -= clickUpgrades.bow.price[1];
+  }
+  countKills();
+  updateSwords();
+}
+function buyspikes() {
+  clickUpgrades.spikes.quantity++;
+  if (clickUpgrades.spikes.quantity <= 10) {
+    orcsSlain -= clickUpgrades.spikes.price[0];
+  } else {
+    orcsSlain -= clickUpgrades.spikes.price[1];
+  }
+  countKills();
+  updateSwords();
+}
+function buycompanions() {
+  autoUpgrades.companions.quantity++;
+}
+// function collectAutoUpgrades() {
+//   orcsSlain += autoUpgrades.quantity * 20;
+// }
+// function startInterval() {
+//   setInterval(collectAutoUpgrades, 3000);
+//   countKills();
+// }
